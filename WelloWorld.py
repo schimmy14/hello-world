3fe2517cc5ca0de16e89d653a9520fc61a8d6519
schimmy14 <schimmy14@gmail.com>
2016-06-09 14:39:04 -0400

    This is a commit message - Tyler

--- WelloWorld.py
+++ WelloWorld.py
@@ -1,1 +1,13 @@
 # This is a practice code for github
+
+import numpy as np
+
+
+A = np.arange(1,7).reshape(2,3)
+
+print A
+B = np.arange(1,7).reshape(3,2)
+print B
+
+print np.dot(A,B)
+print np.dot(B,A)
